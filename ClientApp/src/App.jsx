dbfa1cd6d319eacfa8ba28c2cc8d574180018f52
Josh Mann <jlmann72@gmail.com>
2020-11-03 17:39:42 -0500

    Added Map to page

--- ClientApp/src/App.jsx
+++ ClientApp/src/App.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './custom.scss'
+import ReactMapGL, { NavigationControl } from 'react-map-gl'
 
 export function App() {
+  const [viewport, setViewport] = useState({
+    width: 500,
+    height: 400,
+    latitude: 27.77,
+    longitude: -82.66,
+    zoom: 9.8,
+  })
   return (
-    <div className="d-flex justify-content-center display-2">Hello, World!</div>
+    <>
+      <header>
+        <div className="d-flex justify-content-center display-2 hero-text">Earthquake!</div>
+      </header>
+      <section className="map">
+        <ReactMapGL
+          style={{ position: 'absolute' }}
+          {...viewport}
+          onViewportChange={setViewport}
+          mapboxApiAccessToken={process.env.REACT_APP_MAPBOX_TOKEN}
+          >
+            <div style={{ position: 'absolute', right: 0 }}>
+              <NavigationControl />
+            </div>
+          </ReactMapGL>
+      </section>
+    </>
   )
 }
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Earthquake
+
+<!-- <span>Photo by <a href="https://unsplash.com/@photoart2018?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Andrew Buchanan</a> on <a href="https://unsplash.com/s/photos/earthquake?utm_source=unsplash&amp;utm_medium=referral&amp;utm_content=creditCopyText">Unsplash</a></span> -->
